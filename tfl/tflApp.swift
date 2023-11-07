1f295b78a39c0cfe59c6bf5a6fab60a6cc3e9b10
Brian Halpin <brian.halpin@mac.com>
2023-11-07 19:30:47 +0000

    Initial Commit

---
+++ tfl/tflApp.swift
@@ -0,0 +1,17 @@
+//
+//  tflApp.swift
+//  tfl
+//
+//  Created by Brian Halpin on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct tflApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
